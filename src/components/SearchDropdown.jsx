78f9c3050899d58bafa5728a9e77334575896b6b
Rommel Canoy <rommelcanoy4@gmail.com>
2023-04-10 04:38:35 +0800

    chore: add select make, model, year data in the handleFilter

--- src/components/SearchDropdown.jsx
+++ src/components/SearchDropdown.jsx
@@ -1,13 +1,30 @@
 import React from 'react'
+import { useEffect } from 'react';
 
-const SearchDropdown = ({ type, data }) => {
+const SearchDropdown = ({ type, data, setSelectedValue }) => {
+
+  const handleSelectChange = (e) => {
+    const selectedValue = e.target.value;
+    setSelectedValue(selectedValue);
+  }
+
+  useEffect(() => {
+  }, []);
+
   return (
     <>
       <div className=''>
         <label htmlFor="small" className="block mb-2 text-xs font-medium text-gray-900 dark:text-white">{type}</label>
-        <select id="small" className="block w-full p-2 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+        <select id="small" className="block w-full p-2 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" onChange={handleSelectChange}>
           {/* <option selected>Select</option> */}
-          <option defaultValue="">Select</option>
+          <option defaultValue="">
+            {
+              data.length === 0 ?
+                type === 'Model' ? "Please select 'Make'" : "Please select 'Model'"
+                :
+                `Select ${type}`
+            }
+          </option>
           {
             data && data.map((data) => (
               <option value={data} key={data}>{data}</option>
--- src/components/SearchMMY.jsx
+++ src/components/SearchMMY.jsx
@@ -9,15 +9,37 @@ const SearchMMY = () => {
   const [makes, setMakes] = useState([]);
   const [models, setModels] = useState([]);
   const [years, setYears] = useState([]);
+  const [selectedMake, setSelectedMake] = useState('');
+  const [selectedModel, setSelectedModel] = useState('');
+  const [selectedYear, setSelectedYear] = useState('');
 
   useEffect(() => {
     const makes = _.sortBy([...new Set(carsData.map((car) => car.make))]);
-    const models = _.sortBy([...new Set(carsData.map((car) => car.model))]);
-    const years = _.sortBy([...new Set(carsData.map((car) => car.year))]);
     setMakes(makes);
+  }, [carsData]);
+
+  useEffect(() => {
+    const models = _.sortBy(
+      [...new Set(carsData.filter((car) => car.make === selectedMake).map((car) => car.model))]
+    );
     setModels(models);
+  }, [selectedMake, carsData]);
+
+  useEffect(() => {
+    const years = _.sortBy(
+      [...new Set(carsData.filter((car) => car.make === selectedMake && car.model === selectedModel).map((car) => car.year))]
+    );
     setYears(years);
-  }, []);
+  }, [selectedMake, selectedModel, carsData]);
+
+  const handleFilter = () => {
+    // Use selectedMake, selectedModel, and selectedYear for filtering logic
+    console.log('Selected Make:', selectedMake);
+    console.log('Selected Model:', selectedModel);
+    console.log('Selected Year:', selectedYear);
+
+    // Add your filtering logic here using the selected values
+  }
 
   return (
     <>
@@ -26,13 +48,13 @@ const SearchMMY = () => {
 
         <div className='flex flex-col gap-5'>
           <div className='grid grid-cols-1 md:grid-cols-3 gap-3'>
-            <SearchDropdown type={'Make'} data={makes} />
-            <SearchDropdown type={'Model'} data={models} />
-            <SearchDropdown type={'Year'} data={years} />
+            <SearchDropdown type={'Make'} data={makes} setSelectedValue={setSelectedMake} />
+            <SearchDropdown type={'Model'} data={models} setSelectedValue={setSelectedModel} />
+            <SearchDropdown type={'Year'} data={years} setSelectedValue={setSelectedYear} />
 
           </div>
           <div className='flex'>
-            <button type="button" className="text-white right-2.5 bottom-2.5 bg-gray-900 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-md text-sm px-4 py-2">Filter</button>
+            <button type="button" className="text-white right-2.5 bottom-2.5 bg-gray-900 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-md text-sm px-4 py-2" onClick={handleFilter}>Filter</button>
           </div>
         </div>
       }
